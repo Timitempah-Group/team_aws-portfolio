22762f5100ae535e8a30f2ad772b17d9caa36564
Nehemiah Adeiza <senate@timitempah.com>
2026-07-16 01:14:19 +0100

    Task 10: CI/CD Pipeline with Blue/Green Deployment

---
+++ tasks/10-cicd-pipeline/main.tf
@@ -0,0 +1,100 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  backend "s3" {
+    bucket         = "tfstate-senate-aws-portfolio"
+    key            = "tasks/10-cicd-pipeline/terraform.tfstate"
+    region         = "eu-west-2"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
+  }
+}
+
+provider "aws" {
+  region = "eu-west-2"
+
+  default_tags {
+    tags = {
+      Project     = "aws-portfolio"
+      Task        = "10-cicd-pipeline"
+      Owner       = "senate-adeiza"
+      Environment = "portfolio"
+    }
+  }
+}
+
+resource "aws_iam_role" "codedeploy" {
+  name = "portfolio-codedeploy-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "codedeploy.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "codedeploy" {
+  role       = aws_iam_role.codedeploy.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSCodeDeployRole"
+}
+
+resource "aws_codedeploy_app" "web_tier" {
+  name             = "portfolio-web-tier-app"
+  compute_platform = "Server"
+}
+
+resource "aws_codedeploy_deployment_group" "web_tier" {
+  app_name              = aws_codedeploy_app.web_tier.name
+  deployment_group_name = "portfolio-web-tier-dg"
+  service_role_arn      = aws_iam_role.codedeploy.arn
+
+  autoscaling_groups = ["portfolio-web-asg"]
+
+  deployment_config_name = "CodeDeployDefault.AllAtOnce"
+
+  deployment_style {
+    deployment_option = "WITH_TRAFFIC_CONTROL"
+    deployment_type   = "BLUE_GREEN"
+  }
+
+  blue_green_deployment_config {
+    terminate_blue_instances_on_deployment_success {
+      action                           = "TERMINATE"
+      termination_wait_time_in_minutes = 5
+    }
+
+    deployment_ready_option {
+      action_on_timeout = "CONTINUE_DEPLOYMENT"
+    }
+
+    green_fleet_provisioning_option {
+      action = "COPY_AUTO_SCALING_GROUP"
+    }
+  }
+
+  load_balancer_info {
+    target_group_info {
+      name = "portfolio-web-tg"
+    }
+  }
+}
+
+output "codedeploy_app_name" {
+  value = aws_codedeploy_app.web_tier.name
+}
+
+output "codedeploy_deployment_group" {
+  value = aws_codedeploy_deployment_group.web_tier.deployment_group_name
+}
